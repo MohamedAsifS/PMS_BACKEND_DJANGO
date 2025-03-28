3c6244071550809f1e174a8c0a4da36222c3e83c
Mohamed  Asif S <maasifar@gmail.com>
2025-03-28 06:30:33 +0530

    added staticfiles and chnaged database to railway

--- PMS/PMS/settings.py
+++ PMS/PMS/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import datetime
 from environ import Env 
 import dj_database_url
+import os
 
 env = Env()
 Env.read_env()
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
